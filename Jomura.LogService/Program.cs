accca93a1c5d67dff1a42bf5cd95c640c3854540
jomora <jomora@8baf125f-01ad-5848-aa1a-46a96bbd3af2>
2009-06-17 14:33:36 +0000

    Jomura.LogServiceの追加 #69

---
+++ Jomura.LogService/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.ServiceProcess;
+using System.Text;
+
+namespace LogService
+{
+    static class Program
+    {
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+
+            // 複数のユーザー サービスが同じプロセスで実行されている可能性があります。
+            // このプロセスにもう 1 つサービスを追加するには、次の行を変更して 2 番目の
+            // サービス オブジェクトを作成してください。たとえば、以下のとおりです。
+            //
+            //   ServicesToRun = new ServiceBase[] {new Service1(), new MySecondUserService()};
+            //
+            ServicesToRun = new ServiceBase[] { new RemoteLoggingService() };
+
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
